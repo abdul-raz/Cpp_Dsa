62f6b49babb1a7beef35b15d734b7e9f59740758
abdul-raz <142838157+abdul-raz@users.noreply.github.com>
2025-05-15 23:08:31 +0530

    Add files via upload

---
+++ Set_2_Prob_2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int x;
+	cin>>x;
+	for(int i = x;i<=1000;i=i+x){
+		cout<<i<<endl;
+	}
+}
---
+++ Set_2_Prob_3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	string fname,lname;
+	int a;
+	cin>>fname>>lname>>a;
+	for(int i =0;i<a;i++){
+		cout<<fname+lname<<endl;
+	}
+	return 0;
+}
